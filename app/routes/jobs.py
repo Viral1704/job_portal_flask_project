f5b12f95ee35e6c56e28bdc44577dfbe6f2ec772
Viral_1704 <viralvaghasiya8@gmail.com>
2026-02-26 12:33:07 +0530

    Now recruiter can see that applicants details who applied for their job.

--- app/routes/jobs.py
+++ app/routes/jobs.py
@@ -6,6 +6,7 @@ from app.extensions import db
 
 from app.models.user import User
 from app.models.job import Job
+from app.models.application import Application
 
 
 job_bp = Blueprint('job', __name__)
@@ -100,3 +101,42 @@ def get_my_jobs():
         })
 
     return jsonify({'jobs' : jobs_data}), 200
+
+
+
+@job_bp.route('/jobs/<int:job_id>/applicants', methods = ['GET'])
+@jwt_required()
+def recruiter_get_users_applied_for_job(job_id):
+
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({'error' : "User not found!"}), 404
+    
+    if user.role != 'recruiter':
+        return jsonify({'error' : "This user not access to fetch the data!"}), 403
+    
+    job = Job.query.get(job_id)
+
+    if not job:
+        return jsonify({'message' : 'Job must required!'}),404
+
+    if job.recruiter_id != user.id:
+        return jsonify({'message' : 'You are not belongs to this job!'}), 403
+    
+    applications = job.applications.all()
+
+    applicant_data = []
+
+    for application in applications:
+        candidate = application.candidate
+        applicant_data.append({
+            'applicant_id' : candidate.id,
+            'applicant_name' : candidate.name,
+            'applicant_email' : candidate.email,
+            'application_status' : application.status,
+            'applied_date' : application.created_at.isoformat()
+        })
+
+    return jsonify({'applicants' : applicant_data}), 200
